6c94e6e47d27505ad34f3f26862013fbc38ced56
Sam Ruby <rubys@intertwingly.net>
2008-06-19 14:12:05 -0400

    Bypass authentication when running in 'test' environment

--- lib/admin_auth.rb
+++ lib/admin_auth.rb
@@ -3,6 +3,7 @@ private
 
   # not exactly enterprise grade security, but good enough for demo purposes
   def authenticate
+    return true if RAILS_ENV == 'test'
     authenticate_or_request_with_http_basic("Intertwingly") do |user, password|
       password.crypt('intertwingly') == "infJpuvriVT0o"
     end
